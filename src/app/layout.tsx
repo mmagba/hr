a0c04637acbaaf0c06851bb3d930a2d8f4740d31
Mahmoud Agbaria <mahmoud.moaad@gmail.com>
2026-03-10 05:41:16 +0200

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Heebo } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const heebo = Heebo({
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={`${heebo.variable} antialiased font-sans`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
